a7da759a2aa5f17bd107c1b88342a8de817125ab
kmodelewski <kmodelewski@gmail.com>
2024-07-29 09:02:49 +0200

    commit from intellij

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -2,6 +2,8 @@ package classes;
 
 public class Vehicle {
     public static void main(String[] args) {
+        System.out.println("Hello100");
+        System.out.println("Nowa zmiana");
 
     }
 }
