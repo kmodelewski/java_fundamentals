544dd1318ec1096b53672f9fece3423047c45472
kmodelewski <kmodelewski@gmail.com>
2024-08-01 14:47:08 +0200

    od_main

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,11 +1,7 @@
 package classes;
 
 public class Vehicle {
-    public static void main(String[] args) {
-        System.out.println("Hello100");
-        System.out.println("Nowa zmiana");
-        //git reset --soft
-
+    public void od_main(){
 
     }
 }
